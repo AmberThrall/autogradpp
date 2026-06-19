da9b672a29b495b9eaf732e2668c44c69e5b6d68
Amber Thrall <amber.rose.thrall@gmail.com>
2026-06-19 13:41:48 -0700

    Project setup

---
+++ examples/basic.cpp
@@ -0,0 +1,9 @@
+#include <autogradpp/autogradpp.hpp>
+
+#include <iostream>
+
+int main() {
+    std::cout << "autogradpp version: " << autogradpp::version() << std::endl;
+
+    return 0;
+}
---
+++ include/autogradpp/autogradpp.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace autogradpp {
+    /// Returns the library version as a string.
+    inline std::string version() {
+        return "0.1.0";
+    }
+}
